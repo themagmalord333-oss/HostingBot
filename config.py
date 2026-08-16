55ba44c380dc52d4f8218b0bcfb5f11a4ad8a815
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-08-16 14:36:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "123456")) # Apna default ID daal sakte ho
+API_ID = int(os.getenv("API_ID", "123456"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # ================= SCALER & CLOUD VARIABLES =================
 MONGO_URI = os.getenv("MONGO_URI", "")
 GH_PERSONAL_TOKEN = os.getenv("GH_PERSONAL_TOKEN", "")
-GITHUB_REPO = os.getenv("GITHUB_REPO", "") # Ex: Ziran/Anysnap-Runner
-WORKFLOW_FILE = os.getenv("WORKFLOW_FILE", "main.yml")
+
+# GitHub Secret ka actual naam REPO_NAME rahega
+GITHUB_REPO = os.getenv("REPO_NAME", "").strip()
+
+WORKFLOW_FILE = os.getenv("WORKFLOW_FILE", "main.yml").strip()
